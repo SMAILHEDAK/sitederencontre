e15b7480f2857a1245d37d488293bb6620c4c8dd
Hedak Smail <hedak.smail@gmail.com>
2021-01-26 11:43:05 +0100

    Ajout de la navbar sticky et responsive version non terminée

--- views/lovers.php
+++ views/lovers.php
@@ -1,3 +1,39 @@
 <?php
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"> 
+    <title>Made with "LOVE"</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bonjour (insérer le cookie nom d'utilisateur) <3</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lovers.php">Nos célibataires</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="user.php">Profil</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
